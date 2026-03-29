6b0a342c39876d7b21347c8a01d5a946481706d9
ASHIKA <siranashika07_bai28@mepcoeng.ac.in>
2026-03-29 23:57:59 +0530

    Create letter_combinations_of_a_phone_number.py

---
+++ letter_combinations_of_a_phone_number.py
@@ -0,0 +1,22 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> list[str]:
+        if not digits:
+            return []
+            
+        digitToChar = {
+            "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
+            "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
+        }
+        
+        res = []
+        
+        def backtrack(i, curStr):
+            if len(curStr) == len(digits):
+                res.append(curStr)
+                return
+            
+            for c in digitToChar[digits[i]]:
+                backtrack(i + 1, curStr + c)
+        
+        backtrack(0, "")
+        return res
